e065ed14e24e35100b49e10cc1cbb56c1c201d43
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-09-04 13:23:36 -0500

    chore: comment cli generated tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { NGXDEMOPage } from './app.po';
+// import { NGXDEMOPage } from './app.po';
 
-describe('ngx-demo App', () => {
-  let page: NGXDEMOPage;
+// describe('ngx-demo App', () => {
+//   let page: NGXDEMOPage;
 
-  beforeEach(() => {
-    page = new NGXDEMOPage();
-  });
+//   beforeEach(() => {
+//     page = new NGXDEMOPage();
+//   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
-});
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('Welcome to app!');
+//   });
+// });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
-import { browser, by, element } from 'protractor';
+// import { browser, by, element } from 'protractor';
 
-export class NGXDEMOPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+// export class NGXDEMOPage {
+//   navigateTo() {
+//     return browser.get('/');
+//   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
-}
+//   getParagraphText() {
+//     return element(by.css('app-root h1')).getText();
+//   }
+// }
